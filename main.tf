722f9541435322e8a6aa699389c8c37e4f4bdd65
Ahmad Naeem <38937613+ahmadnaeem2002@users.noreply.github.com>
2023-07-23 10:29:11 +0300

    Add files via upload

--- main.tf
+++ main.tf
@@ -1,30 +1,104 @@
 provider "aws" {
     region = "us-east-1"
 }
-variable "sub_sider_block" {
-  description = "subnet1 sider block"
-}
-resource "aws_vpc" "dev-vpc" {
-  cidr_block = "10.0.0.0/16"
+variable sub_sider_block {}
+variable vpc_sider_block {}
+variable avail_zone {}
+variable env_prefix {}
+variable my_ip {}
+variable instance_type {}
+resource "aws_vpc" "myapp-vpc" {
+  cidr_block = var.vpc_sider_block
   tags = {
-    Name: "dev-vpc"
-    vpc_env: "dev"
+    Name: "${var.env_prefix}-vpc"
   }
 }
-
-resource "aws_subnet" "dev-sub1" {
-  vpc_id     = aws_vpc.dev-vpc.id
+resource "aws_internet_gateway" "myapp-igw" {
+  vpc_id = aws_vpc.myapp-vpc.id
+  tags = {
+    Name = "${var.env_prefix}-igw"
+  }
+}
+resource "aws_subnet" "myapp-sub1" {
+  vpc_id     = aws_vpc.myapp-vpc.id
   cidr_block = var.sub_sider_block
   availability_zone = "us-east-1b"
   tags = {
-    Name: "dev-sub1"
+    Name: "${var.env_prefix}-sub1"
   }
 }
-resource "aws_subnet" "dev-sub2" {
-  vpc_id     = aws_vpc.dev-vpc.id
-  cidr_block = "10.0.20.0/24"
-  availability_zone = "us-east-1b"
+resource "aws_route_table" "myapp-rout-table" {
+  vpc_id = aws_vpc.myapp-vpc.id
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.myapp-igw.id
+  }
   tags = {
-    Name: "dev-sub2"
+    Name = "${var.env_prefix}-route-table"
+  }
+}
+resource "aws_route_table_association" "myapp-rta" {
+  subnet_id      = aws_subnet.myapp-sub1.id
+  route_table_id = aws_route_table.myapp-rout-table.id
+}
+
+resource "aws_security_group" "myapp-sg" {
+  name        = "myapp-sg"
+  vpc_id      = aws_vpc.myapp-vpc.id
+
+  ingress {
+    description      = "TLS from VPC"
+    from_port        = 22
+    to_port          = 22
+    protocol         = "tcp"
+    cidr_blocks      = [var.my_ip]
+  }
+  ingress {
+    description      = "TLS from outside"
+    from_port        = 8080
+    to_port          = 8080
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
+  tags = {
+    Name = "${var.env_prefix}-sg"
+  }
+}
+data "aws_ami" "iam" {
+  most_recent = true
+  owners      = ["amazon"]
+  filter {
+    name   = "name"
+    values = ["amzn-ami-*-x86_64-gp2"]
+  }
+  filter {
+    name = "virtualization-type"
+    values = ["hvm"]
+  }
+}
+resource "aws_instance" "myapp-server" {
+  ami = data.aws_ami.iam.id
+  instance_type = var.instance_type
+  subnet_id = aws_subnet.myapp-sub1.id
+  vpc_security_group_ids = [aws_security_group.myapp-sg.id]
+  availability_zone = var.avail_zone
+  associate_public_ip_address = true
+  key_name = "terraform"
+  user_data = <<EOF
+                    #!/bin/bash
+                    sudo yum -y update
+                    sudo yum install -y docker
+                    sudo systemctl start docker 
+                    sudo usermod -aG docker ec2-user
+                    docker run -p 8080:80 nginx 
+              EOF
+  tags = {
+    Name = "${var.env_prefix}-server"
   }
 }
--- terraform.tfvars
+++ terraform.tfvars
@@ -1,2 +1,7 @@
-sub_sider_block = "10.0.10.0/24"
 
+sub_sider_block = "10.0.10.0/24"
+vpc_sider_block = "10.0.0.0/16"
+avail_zone = "us-east-1b"
+env_prefix = "dev"
+my_ip = "0.0.0.0/0"
+instance_type = "t2.micro"
